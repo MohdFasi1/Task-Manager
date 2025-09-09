ddea5ea715d0d88fc28bf1725dbfeb3c699d0c13
MohdFasi1 <mohdfasi7451@gmail.com>
2025-09-10 00:17:15 +0530

    removing waste

--- lib/agenda/jobs/reminder.job.ts
+++
@@ -1,6 +0,0 @@
-import {agenda} from "@/lib/agenda";
-
-agenda.define("send reminder", async (job) => {
-  const { task } = job.attrs.data as { task: string };
-  console.log("🔔 Reminder:", task);
-});
